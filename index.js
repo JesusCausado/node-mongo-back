7c1fc23a17c34cdd8f48c0672ee490d867116852
JesusCausado <jesus-ca-01@hotmail.com>
2020-05-22 17:19:15 -0500

    Readme updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 var app = require('./app');
-var port = 3900;
+var port = 4000;
 
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
